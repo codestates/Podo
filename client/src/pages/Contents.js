6a960f3478e43f7fdf793723c4d078ff4301048d
phosa9203 <phosa9203@gmail.com>
2021-12-23 17:11:06 +0900

    [client] feat: #186 Fix create SCSS/ Add Contents

--- client/src/pages/Contents.js
+++ client/src/pages/Contents.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "../style/Contents.scss";
 import Header from "../components/public/Header";
+import profile5 from "../image/profile5.svg";
 
 function Contents(props) {
   return (
     <>
       <Header />
+      <div className="contentspage">
+        <div className="main">
+          <img src={profile5} alt="profile5"></img>
+          <div>서비스가 준비중입니다</div>
+        </div>
+      </div>
     </>
   );
 }
